1e8f85092bda0dddbdc8f14f397ebc94850a2fec
fsuh <92304761+fsuh@users.noreply.github.com>
2023-11-07 10:17:13 +0200

    updated product page functionality

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -3,21 +3,19 @@ import { Filters, PaginationContainer, ProductsContainer } from "../components";
 import { customFetch } from "../utils";
 const url = "/products";
 
-export const loader =
-	() =>
-	async ({ request }: { request: Request }) => {
-		// const params = new URL(request.url).searchParams;
-		// const search = params.get("search");
-		// console.log(search);
-		const params = Object.fromEntries([
-			...new URL(request.url).searchParams.entries(),
-		]);
-		//console.log(params);
-		const response = await customFetch(url, { params });
-		const products = response.data.data;
-		const meta = response.data.meta;
-		return { products, meta, params };
-	};
+export const loader = async ({ request }: { request: Request }) => {
+	// const params = new URL(request.url).searchParams;
+	// const search = params.get("search");
+	// console.log(search);
+	const params = Object.fromEntries([
+		...new URL(request.url).searchParams.entries(),
+	]);
+	//console.log(params);
+	const response = await customFetch(url, { params });
+	const products = response.data.data;
+	const meta = response.data.meta;
+	return { products, meta, params };
+};
 const Products = () => {
 	return (
 		<>
